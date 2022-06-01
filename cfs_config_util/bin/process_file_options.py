5427fdd14e0e90481f140771537cc042385027be
Ryan Haasken <ryan.haasken@hpe.com>
2022-06-01 17:01:13 -0500

    CRAYSAT-1418: Add LICENSE file and update every source file
    Add a LICENSE file at the top level of the repository that includes the
    MIT license text, and include that same license text in every source
    file in the format expected by the license checker GitHub action.
    
    Also update all copyright years to include the current year, 2022,
    because this is required by the license checker.
    
    Test Description:
    Check if it passes the license checker.

--- cfs_config_util/bin/process_file_options.py
+++ cfs_config_util/bin/process_file_options.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# (C) Copyright 2022 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
 """
 Entry point used by product installers to determine mounts needed to run in container.
 
@@ -9,8 +31,6 @@ file paths replaced with paths inside the container environment.
 
 This information is dumped in JSON format so it can easily be accessed in a shell
 script using the `jq` utility.
-
-Copyright 2022 Hewlett Packard Enterprise Development LP
 """
 from dataclasses import dataclass
 import json
